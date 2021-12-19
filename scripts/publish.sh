549ca81f4834a4187059d25d0645778a68846d68
MustansirZia <mustansir29@hotmail.com>
2021-12-19 22:58:20 +0530

    feat: test workflow

---
+++ scripts/publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Build variables.
+export ENVIRONMENT=production
+
+# Deployment variables.
+export SERVER_HOST=68.183.83.10
+export SERVER_DIR=/var/www/html 
+export SERVER_USER=root
+
+
+# Build app.
+echo "Building the production build for the app...."
+    npm run build --scripts-prepend-node-path &&
+    echo "Production build for the app built!" &&
+    echo "Uploading files to web server..." &&
+    echo 'put -r out/*' | sftp $SERVER_USER@$SERVER_HOST:$SERVER_DIR &&
+    echo "Uploaded files to web server!"    
